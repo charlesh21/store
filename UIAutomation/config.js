5f307b7ee41aa79df1793f637db0bdb5a38360b8
winterya <winterya@yahoo-inc.com>
2014-05-08 16:38:59 +0800

    update config.js

--- UIAutomation/config.js
+++ UIAutomation/config.js
@@ -15,9 +15,6 @@ var Assert = {};
 #import "smoke/Action/ItemPage.js"
 #import "smoke/Action/ShoppingCartlist.js"
 #import "smoke/Action/StoreListings.js"
-#import "Function/Action/Category.js"
-#import "Function/Action/Search.js"
-#import "Function/Action/Welcome.js"
 
 //Assert
 #import "smoke/Assert/FavoriteStores.js"
@@ -35,16 +32,13 @@ var Assert = {};
 #import "smoke/Assert/ShoppingCartlist.js"
 #import "smoke/Assert/StoreListings.js"
 #import "smoke/Assert/Search.js"
-#import "Function/Assert/Category.js"
-#import "Function/Assert/Search.js"
-#import "Function/Assert/Welcome.js"
 
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var tabBar = app.tabBar();
-var mainWindow = app.mainWindow();
+var window = app.mainWindow();
 var navBar = app.navigationBar();
-var statusBar = app.statusBar();
+var sBar = app.statusBar();
 
 var obj = {};
 var sleep = 2;
@@ -58,6 +52,31 @@ var tabBarMyAccount = tabBar.buttons()[4];
 obj.scrollDowns = function (total) {
     for (var j = 0; j < total; j++) {
         $.delay(sleep);
-        app.mainWindow().collectionViews()[0].scrollDown();
+        window.collectionViews()[0].scrollDown();
     }
 };
+obj.checkInstanceExist = function (instance) {
+    var errorTimes = 0;
+    while (!instance && errorTimes < 50) {
+        errorTimes++;
+        target.delay(1);
+    }
+};
+obj.logDevice = function () {
+    UIALogger.logMessage("Dump Device:");
+    UIALogger.logMessage(" model: " + UIATarget.localTarget().model());
+    UIALogger.logMessage(" rect: " + JSON.stringify(UIATarget.localTarget().rect()));
+    UIALogger.logMessage(" name: "+ UIATarget.localTarget().name());
+    UIALogger.logMessage(" systemName: "+ UIATarget.localTarget().systemName());
+    UIALogger.logMessage(" systemVersion: "+ UIATarget.localTarget().systemVersion());
+};
+
+UIATarget.onAlert = function onAlert(alert) {
+    var title = alert.name();
+    UIALogger.logWarning("Alert with title '" + title + "' encountered!");
+    UIATarget.localTarget().captureScreenWithName("alert_" + (new Date()).UTC());
+    // test if your script should handle the alert, and if so, return true
+    // otherwise, return false to use the default handler
+    return false;
+}
+obj.logDevice();
